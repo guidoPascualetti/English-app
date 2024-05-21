b3c0b3d91add8f9cdfd39f84fd96e828bb2321be
Guido Pascualetti <pascualettiguido@gmail.com>
2024-05-21 19:40:27 -0300

    Initial

--- be/index.js
+++ be/index.js
@@ -2,11 +2,13 @@ import app from "./src/server.js";
 // import knex from "knex";
 const SERVER_PORT = 3000;
 
+const BACKEND_PORT = process.env.BACKEND_PORT
 // const db_instance = knex(knexConfig);
-
+// saasaas
+// dasdsa
 // global.db_con = db_instance;
 // global.User = global.db_con("users");
 // global.Word = global.db_con("words");
-app.listen(SERVER_PORT, async () => {
-  console.log(`server port: ${SERVER_PORT}`);
+app.listen(BACKEND_PORT, async () => {
+  console.log(`server port: ${BACKEND_PORT}`);
 });
